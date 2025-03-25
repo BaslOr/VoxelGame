1fa8256fe3248f0e2fadff2e6eb9d1f8d3665eda
BaslOr <bastian.schwarzmann@web.de>
2025-03-25 20:01:40 +0100

    Quick refactoring

--- Cubes Editor/src/Camera.cpp
+++ Cubes Editor/src/Camera.cpp
@@ -1,67 +1,69 @@
 #include "Camera.h"
 
+namespace Cubes {
 
-Camera::Camera()
-{
-}
-
-Camera::Camera(glm::vec3& position, float fov)
-    : PerspectiveCamera(position, (float)Cubes::Application::Get().GetWindow().GetWidth() / (float)Cubes::Application::Get().GetWindow().GetHeight(), fov), _fov(fov), _position(position)
-{
-    SetDirection(glm::vec3(0.f, 0.f, -1.f));
-}
-
-void Camera::OnUpdate(Cubes::TimeStep deltaTime)
-{
-    //Only do Mouse calculations when right mouse Button is clicked
-    bool doMouseCalculations = false;
-    if (Cubes::Input::IsMouseButtonDown(CB_MOUSE_BUTTON_RIGHT)) {
-        Cubes::Application::Get().GetWindow().SetInputModeCursor(true);
-        doMouseCalculations = true;
+    Camera::Camera()
+    {
     }
-    else
-        Cubes::Application::Get().GetWindow().SetInputModeCursor(false);
+
+    Camera::Camera(glm::vec3& position, float fov)
+        : PerspectiveCamera(position, (float)Cubes::Application::Get().GetWindow().GetWidth() / (float)Cubes::Application::Get().GetWindow().GetHeight(), fov), _fov(fov), _position(position)
+    {
+        SetDirection(glm::vec3(0.f, 0.f, -1.f));
+    }
+
+    void Camera::OnUpdate(Cubes::TimeStep deltaTime)
+    {
+        //Only do Mouse calculations when right mouse Button is clicked
+        bool doMouseCalculations = false;
+        if (Cubes::Input::IsMouseButtonDown(CB_MOUSE_BUTTON_RIGHT)) {
+            Cubes::Application::Get().GetWindow().SetInputModeCursor(true);
+            doMouseCalculations = true;
+        }
+        else
+            Cubes::Application::Get().GetWindow().SetInputModeCursor(false);
 
 
-    float moveDistance = _movementSpeed * deltaTime;
+        float moveDistance = _movementSpeed * deltaTime;
 
 
-    if (Cubes::Input::IsKeyDown(CB_KEY_W))
-        _position += _front * _movementSpeed;
-    else if (Cubes::Input::IsKeyDown(CB_KEY_S))
-        _position -= _front * _movementSpeed;
-    if (Cubes::Input::IsKeyDown(CB_KEY_D))
-        _position += _right * _movementSpeed;
-    else if (Cubes::Input::IsKeyDown(CB_KEY_A))
-        _position -= _right * _movementSpeed;
-    if (Cubes::Input::IsKeyDown(CB_KEY_SPACE))
-        _position.y += _movementSpeed;
-    else if (Cubes::Input::IsKeyDown(CB_KEY_LEFT_SHIFT))
-        _position.y -= _movementSpeed;
+        if (Cubes::Input::IsKeyDown(CB_KEY_W))
+            _position += _front * _movementSpeed;
+        else if (Cubes::Input::IsKeyDown(CB_KEY_S))
+            _position -= _front * _movementSpeed;
+        if (Cubes::Input::IsKeyDown(CB_KEY_D))
+            _position += _right * _movementSpeed;
+        else if (Cubes::Input::IsKeyDown(CB_KEY_A))
+            _position -= _right * _movementSpeed;
+        if (Cubes::Input::IsKeyDown(CB_KEY_SPACE))
+            _position.y += _movementSpeed;
+        else if (Cubes::Input::IsKeyDown(CB_KEY_LEFT_SHIFT))
+            _position.y -= _movementSpeed;
 
-    SetPosition(_position);
+        SetPosition(_position);
 
-    if (!doMouseCalculations)
-        return;
+        if (!doMouseCalculations)
+            return;
 
-    float currentXPosition = Cubes::Input::GetMouseX();
-    float currentYPosition = Cubes::Input::GetMouseY();
-    float deltaX = currentXPosition - _lastMouseX;
-    float deltaY = _lastMouseY - currentYPosition;
-    _lastMouseX = currentXPosition;
-    _lastMouseY = currentYPosition;
+        float currentXPosition = Cubes::Input::GetMouseX();
+        float currentYPosition = Cubes::Input::GetMouseY();
+        float deltaX = currentXPosition - _lastMouseX;
+        float deltaY = _lastMouseY - currentYPosition;
+        _lastMouseX = currentXPosition;
+        _lastMouseY = currentYPosition;
 
-    _yaw += deltaX * _mouseSensitivity;
-    _pitch += deltaY * _mouseSensitivity;
+        _yaw += deltaX * _mouseSensitivity;
+        _pitch += deltaY * _mouseSensitivity;
 
-    _pitch = glm::clamp(_pitch, -90.f, 90.f);
+        _pitch = glm::clamp(_pitch, -90.f, 90.f);
 
-    glm::vec3 direction;
-    direction.x = cos(glm::radians(_yaw)) * cos(glm::radians(_pitch));
-    direction.y = sin(glm::radians(_pitch));
-    direction.z = sin(glm::radians(_yaw)) * cos(glm::radians(_pitch));
-    _front = glm::normalize(direction);
+        glm::vec3 direction;
+        direction.x = cos(glm::radians(_yaw)) * cos(glm::radians(_pitch));
+        direction.y = sin(glm::radians(_pitch));
+        direction.z = sin(glm::radians(_yaw)) * cos(glm::radians(_pitch));
+        _front = glm::normalize(direction);
 
-    SetDirection(direction);
+        SetDirection(direction);
+    }
+
 }
-
--- Cubes Editor/src/Camera.h
+++ Cubes Editor/src/Camera.h
@@ -1,24 +1,26 @@
 #pragma once
 #include <Cubes.h>
 
+namespace Cubes {
 
-class Camera : public Cubes::PerspectiveCamera
-{
-public:
-	Camera();
-	Camera(glm::vec3& position, float fov);
+	class Camera : public PerspectiveCamera
+	{
+	public:
+		Camera();
+		Camera(glm::vec3& position, float fov);
 
-	void OnUpdate(Cubes::TimeStep timeStep);
+		void OnUpdate(TimeStep timeStep);
 
-private:
-	glm::vec3 _position;
-	float _fov;
+	private:
+		glm::vec3 _position;
+		float _fov;
 
-	float _lastMouseX = 0.f, _lastMouseY = 0.f;
-	float _yaw = -90.f, _pitch = 0.f;
-	
+		float _lastMouseX = 0.f, _lastMouseY = 0.f;
+		float _yaw = -90.f, _pitch = 0.f;
 
-	float _movementSpeed = 0.02f;
-	float _mouseSensitivity = 0.05f;
-};
 
+		float _movementSpeed = 0.02f;
+		float _mouseSensitivity = 0.05f;
+	};
+
+}
