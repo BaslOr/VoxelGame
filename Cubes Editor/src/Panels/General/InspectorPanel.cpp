c14081dde91718e0f1f8a3967e02e0d2e4a9d96b
BaslOr <bastian.schwarzmann@web.de>
2025-04-17 12:50:56 +0200

    Created ImGui Lib

--- Cubes Editor/src/Panels/General/InspectorPanel.cpp
+++ Cubes Editor/src/Panels/General/InspectorPanel.cpp
@@ -1,9 +1,8 @@
 #include "InspectorPanel.h"
+#include "../../Utility/ImGuiTemplates.h"
 #include <Cubes/Core/Project/Components.h>
 
 #include <imgui.h>
-#include <imgui_internal.h>
-#include <glm/gtc/type_ptr.hpp>
 
 namespace Cubes {
 
@@ -48,75 +47,6 @@ namespace Cubes {
             entity.RemoveComponent<ComponentType>();
     }
 
-    static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columbWidth = 100.0f)
-    {
-        ImGuiIO& io = ImGui::GetIO();
-        auto boldFont = io.Fonts->Fonts[0];
-
-        ImGui::PushID(label.c_str());
-
-        ImGui::Columns(2);
-        ImGui::SetColumnWidth(0, columbWidth);
-        ImGui::Text(label.c_str());
-        ImGui::NextColumn();
-
-        ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
-
-        float fontScale = io.FontGlobalScale;
-        float lineHeight = GImGui->Font->FontSize * fontScale + GImGui->Style.FramePadding.y * 2.0f;
-        ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
-
-        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
-        ImGui::PushFont(boldFont);
-        if (ImGui::Button("X", buttonSize))
-            values.x = resetValue;
-        ImGui::PopFont();
-        ImGui::PopStyleColor(3);
-
-        ImGui::SameLine();
-        ImGui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f");
-        ImGui::PopItemWidth();
-        ImGui::SameLine();
-
-        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.3f, 1.0f });
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
-        ImGui::PushFont(boldFont);
-        if (ImGui::Button("Y", buttonSize))
-            values.y = resetValue;
-        ImGui::PopFont();
-        ImGui::PopStyleColor(3);
-
-        ImGui::SameLine();
-        ImGui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f");
-        ImGui::PopItemWidth();
-        ImGui::SameLine();
-
-        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25, 0.8f, 1.0f });
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.35f, 0.9f, 1.0f });
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
-        ImGui::PushFont(boldFont);
-        if (ImGui::Button("Z", buttonSize))
-            values.z = resetValue;
-        ImGui::PopFont();
-        ImGui::PopStyleColor(3);
-
-        ImGui::SameLine();
-        ImGui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f");
-        ImGui::PopItemWidth();
-
-        ImGui::PopStyleVar();
-
-        ImGui::Columns(1);
-
-        ImGui::PopID();
-    }
-
-
-
 	InspectorPanel::InspectorPanel()
 		: Panel("Inspector")
 	{
@@ -151,10 +81,10 @@ namespace Cubes {
 
         DrawComponent<TransformComponent>("Transform", _selectedEntity, [](auto& component) {
 
-            DrawVec3Control("Position", component.Position);
+            ImGuiTemplates::DrawVec3Control("Position", component.Position);
             glm::vec3 rotation = glm::degrees(component.Rotation);
-            DrawVec3Control("Rotation", rotation);
-            DrawVec3Control("Scale", component.Scale, 1.0f);
+            ImGuiTemplates::DrawVec3Control("Rotation", rotation);
+            ImGuiTemplates::DrawVec3Control("Scale", component.Scale, 1.0f);
             });
         DrawComponent<CameraComponent>("Camera", _selectedEntity, [](auto& component) {
 
